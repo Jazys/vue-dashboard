586ab63e89551003464eb2f0742c455b0ca60f0e
Jazys <jacquemet.julien@gmail.com>
2024-05-02 23:46:23 +0200

    if user not auth in Kobble need to relaunch authorize, need to set in Header or Dashboard not in Credit.vue

--- src/components/Credits.vue
+++ src/components/Credits.vue
@@ -21,12 +21,23 @@ watch(() => requestStore.loading, async (loading) => {
 onMounted(async () => {
   const isAuth = await kobbleClient.isAuthenticated()
   if (isAuth) {
+    await kobbleClient.refreshAccessTokenIfExpired()
     const quotaLeft = await kobbleClient.acl.listQuotas()
     credits.value = quotaLeft[0].usage
     creditsLimit.value = quotaLeft[0].limit
   }
   else {
-    await kobbleClient.logout()
+    setTimeout(async () => {
+      const isAuth = await kobbleClient.isAuthenticated()
+      if (isAuth === false) {
+        await kobbleClient.loginWithRedirect()
+      }
+      else {
+        const quotaLeft = await kobbleClient.acl.listQuotas()
+        credits.value = quotaLeft[0].usage
+        creditsLimit.value = quotaLeft[0].limit
+      }
+    }, 5000)
   }
 })
 </script>
